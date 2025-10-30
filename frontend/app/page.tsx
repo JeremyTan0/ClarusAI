1c8d9678503ef7c8d6b1b451ceb952ddfb742755
BaronViper <baron.viper10@gmail.com>
2025-10-30 17:08:57 +0800

    Frontend and backend initial commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { useEffect, useState } from 'react'
+
+export default function Home() {
+  const [message, setMessage] = useState('Loading...')
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/hello/')
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+  }, [])
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h1 className="text-2xl font-bold">{message}</h1>
+      </main>
+    </div>
+  );
+}
