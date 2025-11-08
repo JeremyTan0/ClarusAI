f35454f7781f5405e340fde9d5071cfd3bc1dc91
YuPheng Xiong <xiongyup@oregonstate.edu>
2025-11-07 21:01:24 -0800

    Developed a about page and navigation system from going from 1 tab to another

---
+++ frontend/app/stocks/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { useEffect, useState } from 'react'
+
+export default function stocks() {
+    return (
+        <div>
+            <h2>Stocks Page</h2>
+        </div>
+    )
+}
