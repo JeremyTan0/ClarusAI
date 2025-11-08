4acab52c4a6d06e7b2ce5db05ca1d075eca0fffc
YuPheng Xiong <xiongyup@oregonstate.edu>
2025-11-07 21:19:04 -0800

    Fix some errors

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
 export default function about() {
@@ -7,14 +8,19 @@ export default function about() {
             <h2>About Page</h2>
             <article>
                 <h3>Author's</h3>
-                    <p>Daniel Burrows, Fuhei Feng, Jermey Tan, Leah Rosenbaum, YuPheng Xiong</p>
+                <p>Daniel Burrows, Fuhei Feng, Jermey Tan, Leah Rosenbaum, YuPheng Xiong</p>
                 <h3>Summary</h3>
-                    <p>
-                        This webpage is a simple and easy to learn stock investment website. This website was created
-                        the idea that from beginner to expert level of investment, anyone should be able to learn and
-                        figure out how to invest. This website has the abilty to allow users to browse the current
-                        stock market, and 
-                    </p>
+                <p>
+                    This webpage is a simple and easy to learn stock investment website. This website was created
+                    the idea that from beginner to expert level of investment, anyone should be able to learn and
+                    figure out how to invest. This website has the abilty to allow users to browse the current
+                    stock market, and
+                </p>
+                <nav>
+                    <Link href="/">Home</Link>
+                    <Link href="/stocks">Stocks</Link>
+                    <Link href="/about">About</Link>
+                </nav>
             </article>
         </div>
     )
--- frontend/app/stocks/page.tsx
+++ frontend/app/stocks/page.tsx
@@ -1,10 +1,16 @@
 'use client'
+import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
 export default function stocks() {
     return (
         <div>
             <h2>Stocks Page</h2>
+            <nav>
+                <Link href="/">Home</Link>
+                <Link href="/stocks">Stocks</Link>
+                <Link href="/about">About</Link>
+            </nav>
         </div>
     )
 }
