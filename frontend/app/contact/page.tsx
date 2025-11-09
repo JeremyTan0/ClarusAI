8e662b6be31f65e86c3e364e54b0a07bff46d2e8
YuPheng Xiong <xiongyup@oregonstate.edu>
2025-11-09 13:15:06 -0800

    Fixed contact and other page

---
+++ frontend/app/contact/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { useEffect, useState } from 'react'
+
+export default function stocks() {
+    return (
+        <div>
+            <main>
+                    <h2>Contact Page</h2>
+                    <p>This is a placeholder</p>
+            </main>
+
+        </div>
+    )
+}
