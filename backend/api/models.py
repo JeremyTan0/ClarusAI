114441625e7058c9b90b735e7f6b68edeb50ed25
Bar0nDev <jtanrev@gmail.com>
2026-01-19 17:22:00 +0800

    Add user and stock models for database initialization

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import UserManager
+import uuid
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    google_sub = models.CharField(max_length=255, unique=True, null=True, blank=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+class Stock(models.Model):
+    symbol = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    exchange = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    sector = models.CharField(max_length=100, blank=True)
+    industry = models.CharField(max_length=100, blank=True)
+    website = models.URLField(blank=True)
+    beta = models.FloatField(null=True)
+
+
+    def __str__(self):
+        return self.symbol
