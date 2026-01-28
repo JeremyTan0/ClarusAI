195b982531e1da67ce8e69e884a06d5bd60dcf2e
lkrosenbaum1 <110206998+lkrosenbaum1@users.noreply.github.com>
2026-01-28 09:19:54 -1000

    Implementation of Gemini AI referencing stock background from Alpha Vantage.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('hello/', views.hello, name='hello'),
     path('stockInfo/<str:ticker>', views.get_stock_info, name='stock_info' )
+    path('aiResponse/<str:ticker>', views.get_ai_response, name='ai_response' )
 ]
